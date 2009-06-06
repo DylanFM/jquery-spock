79ad6ea9829f892c8e10c4343e0005a58c13283c
DylanFM <dylan.fm@gmail.com>
2009-06-07 00:03:26 +1000

    Wraps found items in a span class keyword element.

--- javascripts/jquery.spock.js
+++ javascripts/jquery.spock.js
@@ -4,39 +4,33 @@
  
   $.fn.extend({
     spock: function(options) {
-      
       opts = $.extend({}, $.fn.spock.defaults, options);
-
       // This will be called on an element or collection of elements
-      // Go through each element
       this.each(processElements);
-      
-      console.log(keywords);
-
-      // Set up handlers that when clicked it runs the item through Wolfram Alpha
-
-      // Display the result in a tooltip
-      
     }
   });
 
   $.fn.spock.defaults = {
-    identifierRegexp: /((?:[A-Z]\w+\s?)+)/g
+    identifierRegexp: /((?:[A-Z]\w+\s?)+)/g,
+    processKeyword: processKeyword
   };
   
   function processElements () {
-    // Find anything in the element's text that seems like it's a proper noun
-    match = $(this).text().match(opts.identifierRegexp);
-    // Trim and remove duplicates
-    var seen = [];
-    keywords = jQuery.map(match, function(w) { 
-      var kw = jQuery.trim(w);
-      if (jQuery.inArray(kw,seen) < 0) {
-        seen.push(kw);
-        return kw;
-      }
-      return null;
-    });
+    // Replace any matched items with the results of the function
+    $(this).html($(this).text().replace(opts.identifierRegexp, opts.processKeyword));
+  }
+  
+  function processKeyword (match) {
+    var following_space = false,
+        kw;
+    // We don't want to enclose a trailing space
+    if (/\w+\s/.test(match)) {
+      following_space = true;
+      match = jQuery.trim(match);
+    }
+    kw = '<span class="keyword">'+match+'</span>';
+    if (following_space) kw += ' ';
+    return kw;
   }
   
 })(jQuery);
