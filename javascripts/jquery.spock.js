cbc11c9e694c1c0d9afcefc475a4ebea854a230e
DylanFM <dylan.fm@gmail.com>
2009-06-06 23:47:32 +1000

    Rearranged a little.

--- javascripts/jquery.spock.js
+++ javascripts/jquery.spock.js
@@ -1,29 +1,17 @@
 (function($) {
+  
+  var opts, keywords;
  
   $.fn.extend({
     spock: function(options) {
-      var opts = $.extend({}, $.fn.spock.defaults, options),
-      keywords = [];
+      
+      opts = $.extend({}, $.fn.spock.defaults, options);
 
       // This will be called on an element or collection of elements
       // Go through each element
-      this.each(function() {
-        // Find anything in the element's text that seems like it's a proper noun
-        var content = $(this).text();
-        // Get any keywords
-        match = content.match(opts.identifierRegexp);
-        // Trim and remove duplicates
-        var seen = [];
-        keywords = jQuery.map(match, function(w) { 
-          var kw = jQuery.trim(w);
-          if (jQuery.inArray(kw,seen) < 0) {
-            seen.push(kw);
-            return kw;
-          }
-          return null;
-        });
-        console.log(keywords);
-      });
+      this.each(processElements);
+      
+      console.log(keywords);
 
       // Set up handlers that when clicked it runs the item through Wolfram Alpha
 
@@ -35,5 +23,20 @@
   $.fn.spock.defaults = {
     identifierRegexp: /((?:[A-Z]\w+\s?)+)/g
   };
+  
+  function processElements () {
+    // Find anything in the element's text that seems like it's a proper noun
+    match = $(this).text().match(opts.identifierRegexp);
+    // Trim and remove duplicates
+    var seen = [];
+    keywords = jQuery.map(match, function(w) { 
+      var kw = jQuery.trim(w);
+      if (jQuery.inArray(kw,seen) < 0) {
+        seen.push(kw);
+        return kw;
+      }
+      return null;
+    });
+  }
   
 })(jQuery);
