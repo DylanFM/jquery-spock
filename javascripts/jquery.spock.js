b4eb6adf8879dbda5fb942733a27b858be2524d8
DylanFM <dylan.fm@gmail.com>
2009-06-06 22:05:34 +1000

    Added a barebones plugin for spock.

---
+++ javascripts/jquery.spock.js
@@ -0,0 +1,13 @@
+(function($) {
+ 
+ $.fn.extend({
+   spock: function(options) {
+     opts = $.extend({}, $.fn.spock.defaults, options);
+   }
+ });
+ 
+ $.fn.spock.defaults = {
+   
+ };
+  
+})(jQuery);
