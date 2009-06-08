9ee68920ec4d8f273e05d54fc1df790a0489bc07
DylanFM <dylan.fm@gmail.com>
2009-06-08 20:38:08 +1000

    Fixing up the example.

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,17 +1,21 @@
 $(function() {
-  $("p").spock({ afterFilter: addWikipediaCheck });
+  $("p").spock({ afterFilter: addWikipediaLinks });
 });
 
-function addWikipediaCheck () {
-  $(this).find("span.keyword").click(function() {
+function addWikipediaLinks () {
+  $(this).find("span.keyword").each(function() {
     var term = $(this).text(),
-        wp_url = "http://en.wikipedia.org/w/api.php?action=opensearch&format=json";
+        wp_url = "http://en.wikipedia.org/w/api.php?action=opensearch&format=json",
+        that = this;
+    // Use the Wikipedia API to see if pages exist for this term
     $.getJSON(wp_url+"&search="+term, function(data) {
+      var replacement = term;
+      // If pages exist, then replace the item with a link to its Wikipedia page
       if (data[1].length > 0) {
-        console.log(data[1]);
-      } else {
-        console.log("No entries.");
+        replacement = "<a href=\"http://en.wikipedia.org/wiki/"+term+"\" title=\"Wikipedia entry for &ldquo;"+term+"&rdquo;\" target=\"_blank\" class=\"wikipedia-link\">"+term+"</a>";
       }
+      $(that).replaceWith(replacement);
     });
   });
+  
 }
